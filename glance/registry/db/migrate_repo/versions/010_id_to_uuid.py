7522d77af3dc5c982b2f46ac983c6e4b776cdda5
Brian Lamar <brian.lamar@rackspace.com>
2011-09-27 14:48:37 -0400

    Updated migration to work with MySQL, not SQLite yet.
    Change-Id: Iba1bd316804906f070673de64b991874b640b6e6

--- glance/registry/db/migrate_repo/versions/010_id_to_uuid.py
+++ glance/registry/db/migrate_repo/versions/010_id_to_uuid.py
@@ -15,54 +15,134 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from migrate.changeset import *
-from sqlalchemy import *
-from sqlalchemy.sql import and_, not_
+import migrate
+import sqlalchemy
 
-from glance.registry.db.migrate_repo.schema import (
-    Boolean, DateTime, Integer, String, Text, from_migration_import)
-import glance.common.utils as utils
+import glance.common.utils
 
 
-meta = MetaData()
-new_id = Column('id', String(40), primary_key=True)
-new_members_id = Column('new_images_id', String(40))
-new_properties_id = Column('new_images_id', String(40))
-
-def get_images_table(meta):
-    return Table('images', meta, autoload=True, useexisting=True)
+meta = sqlalchemy.MetaData()
 
 
-def get_image_properties_table(meta):
-    return Table('image_properties', meta, autoload=True, useexisting=True)
+def _get_table(table_name, metadata):
+    """Return a sqlalchemy Table definition with associated metadata."""
+    return sqlalchemy.Table(table_name, metadata, autoload=True)
 
 
-def get_image_members_table(meta):
-    return Table('image_members', meta, autoload=True, useexisting=True)
+def _get_foreign_keys(t_images, t_image_members, t_image_properties):
+    """Retrieve and return foreign keys for members/properties tables."""
+    image_members_fk_name = t_image_members.foreign_keys[0].name
+    image_properties_fk_name = t_image_properties.foreign_keys[0].name
+
+    fk1 = migrate.ForeignKeyConstraint([t_image_members.c.image_id],
+                                       [t_images.c.id],
+                                       name=image_members_fk_name)
+
+    fk2 = migrate.ForeignKeyConstraint([t_image_properties.c.image_id],
+                                       [t_images.c.id],
+                                       name=image_properties_fk_name)
+
+    return fk1, fk2
+
+
+def upgrade_sqlite(migrate_engine):
+    pass
+
+
+def downgrade_sqlite(migrate_engine):
+    pass
 
 
 def upgrade(migrate_engine):
     meta.bind = migrate_engine
-    conn = migrate_engine.connect()
 
-    images = get_images_table(meta)
-    image_properties = get_image_properties_table(meta)
-    image_members = get_image_members_table(meta)
+    dialect_name = migrate_engine.url.get_dialect().name
 
-    images.create_column(new_id, primary_key=True)
-    #image_properties.create_column(new_properties_id)
-    #image_members.create_column(new_members_id)
+    if dialect_name == "sqlite":
+        return upgrade_sqlite(migrate_engine)
 
-    #raise Exception("TEST")
+    fk1, fk2 = _get_foreign_keys(t_images, t_image_members, t_image_properties)
+
+    fk1.drop()
+    fk2.drop()
+
+    t_images.c.id.alter(sqlalchemy.String(36), primary_key=True)
+    t_image_members.c.image_id.alter(sqlalchemy.String(36))
+    t_image_properties.c.image_id.alter(sqlalchemy.String(36))
+
+    for image in migrate_engine.execute(t_images.select()):
+        old_id = image["id"]
+        new_id = glance.common.utils.generate_uuid()
+
+        query = t_images.update().\
+                    where(t_images.c.id==old_id).\
+                    values(id=new_id)
+
+        migrate_engine.execute(query)
+
+        query = t_image_members.update().\
+                    where(t_image_members.c.image_id==old_id).\
+                    values(image_id=new_id)
+
+        migrate_engine.execute(query)
+
+        query = t_image_properties.update().\
+                    where(t_image_properties.c.image_id==old_id).\
+                    values(image_id=new_id)
+
+        migrate_engine.execute(query)
+
+    fk1.create()
+    fk2.create()
 
 
 def downgrade(migrate_engine):
     meta.bind = migrate_engine
 
-    #images = get_images_table(meta)
-    #image_properties = get_image_properties_table(meta)
-    #image_members = get_image_members_table(meta)
+    dialect_name = migrate_engine.url.get_dialect().name
 
-    #images.drop_column(new_id)
-    #image_properties.drop_column(new_properties_id)
-    #image_members.drop_column(new_members_id)
+    if dialect_name == "sqlite":
+        return downgrade_sqlite(migrate_engine)
+
+    t_images = sqlalchemy.Table('images', meta, autoload=True)
+    t_image_members = sqlalchemy.Table('image_members', meta, autoload=True)
+    t_image_properties = sqlalchemy.Table('image_properties',
+                                          meta,
+                                          autoload=True)
+
+    fk1, fk2 = _get_foreign_keys(t_images, t_image_members, t_image_properties)
+
+    fk1.drop()
+    fk2.drop()
+
+    t_images.c.id.alter(sqlalchemy.Integer(), primary_key=True)
+    t_image_members.c.image_id.alter(sqlalchemy.Integer())
+    t_image_properties.c.image_id.alter(sqlalchemy.Integer())
+
+    for image in migrate_engine.execute(t_images.select()):
+        old_id = image["id"]
+        new_id = 0
+
+        query = t_images.update().\
+                    where(t_images.c.id==old_id).\
+                    values(id=new_id)
+
+        migrate_engine.execute(query)
+
+        query = t_image_members.update().\
+                    where(t_image_members.c.image_id==old_id).\
+                    values(image_id=new_id)
+
+        migrate_engine.execute(query)
+
+        query = t_image_properties.update().\
+                    where(t_image_properties.c.image_id==old_id).\
+                    values(image_id=new_id)
+
+        migrate_engine.execute(query)
+
+        new_id += 1
+
+    fk1.create()
+    fk2.create()
+
